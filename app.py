8d8771fc73c0c42c6210eb5617ef89e721beb319
Franco Sbaffi <francosbaffidev@gmail.com>
2025-01-03 03:34:19 -0300

    Commit en 2025-01-03 03:34:19

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from random import randint
 from datetime import datetime, timedelta
 
 # Define la fecha de inicio y fin para la generación de commits (por ejemplo, un año).
-fecha_inicio = datetime(2025, 1, 2)
+fecha_inicio = datetime(2025, 1, 3)
 fecha_fin = datetime(2025, 1, 8)
 
 # Itera para generar commits en fechas aleatorias dentro del rango definido.
